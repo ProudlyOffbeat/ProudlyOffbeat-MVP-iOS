40ef42b87aae8595510343a3803a6f650e38e69e
G.M_YOO <ykm7003@gmail.com>
2026-02-18 01:15:21 +0900

    [Refactor] #PO-44 - StringLiterals로 구조 변경

--- LivingStory-iOS/LivingStory-iOS/Core/Consts/StringLiterals.swift
+++ LivingStory-iOS/LivingStory-iOS/Core/Consts/StringLiterals.swift
@@ -42,4 +42,31 @@ enum StringLiterals {
         static let stopReadingButton = "그만 읽기"
         static let stopReadingAlertMessage = "중단시, 세팅된 환경이 중단될 거에요."
     }
+
+    enum Book {
+        static let title = "책 읽기"
+        static let startButton = "책 읽기 시작하기"
+
+        static let tagUsage = "사용법"
+        static let tag01 = "01"
+        static let tag02 = "02"
+        static let tag03 = "03"
+        static let tag04 = "04"
+
+        static let titleUsage = "책에 맞는 분위기를 만들어\n아이에게 책을 읽어주세요!"
+        static let title01 = "읽을 책의 바코드를 스캔해요"
+        static let title02 = "스캔한 책의 분위기에 맞게\n조명과 스피커가 세팅돼요"
+        static let title03 = "멋진 분위기에서\n아이에게 책을 읽어줘요"
+        static let title04 = "다 읽으면 책에 대해\n아이와 나눌만한 대화를 제안해줘요"
+
+        static let imageUsage = "UsingIntro"
+        static let image01 = "UsingIntro1"
+        static let image02 = "UsingIntro2"
+        static let image03 = "UsingIntro3"
+        static let image04 = "UsingIntro4"
+    }
+
+    enum My {
+        static let title = "마이"
+    }
 }
