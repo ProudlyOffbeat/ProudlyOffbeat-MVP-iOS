fdd9a7ce6acf72cc39d035c1044ee5a1b0c41878
Ito <ckdwo23@gmail.com>
2026-03-19 15:43:38 +0900

    [Style] #PO-80 ReadingView(reading mode) UX 라이팅 변경
    - settingDoneTitle/Subtitle 문구 수정

--- LivingStory-iOS/LivingStory-iOS/Core/Consts/StringLiterals.swift
+++ LivingStory-iOS/LivingStory-iOS/Core/Consts/StringLiterals.swift
@@ -32,8 +32,8 @@ enum StringLiterals {
     enum Reading {
         static let settingTitle = "환경 세팅중..."
         static let settingSubtitle = "몇 분 소요될 수 있습니다."
-        static let settingDoneTitle = "환경이 세팅 됐어요. 이제 재밌게 읽으세요!"
-        static let settingDoneSubtitle = "책 흐름에 맞게 분위기가 변할거에요.\n재밌게 책을 읽어보세요."
+        static let settingDoneTitle = "책 분위기에 맞게 환경이 세팅 됐어요!"
+        static let settingDoneSubtitle = "이제 책을 더 재밌게 읽으세요!"
         static let stopSettingAlert = "환경 세팅을 전체 중단할까요?"
         static let stopReadingAlert = "책 읽기를 그만할까요?"
         static let close = "닫기"
